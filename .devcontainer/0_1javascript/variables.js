db7cbb39131f70acf0a0158f417d57ed5278d96c
Deeppadwale <142868667+Deeppadwale@users.noreply.github.com>
2024-08-08 08:56:06 +0000

    container confic done

---
+++ .devcontainer/0_1javascript/variables.js
@@ -0,0 +1,10 @@
+const accountId=4050;
+let   accountEmail="deeppadwale@gmail.come";
+var   accountPassword ="123"
+      accountCity="kolhapr";
+let   accountAddress;
+
+    //  accountId=5050;
+    accountCity="kolhapur"
+
+     console.table([accountId,accountEmail,accountPassword,accountCity,accountAddress])
