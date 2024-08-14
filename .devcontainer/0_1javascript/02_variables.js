8b90e7117b1bdc8b61da339c26d67aa1d5590fbe
Deeppadwale <142868667+Deeppadwale@users.noreply.github.com>
2024-08-14 08:18:18 +0000

    container

--- .devcontainer/0_1javascript/02_variables.js
+++ .devcontainer/0_1javascript/02_variables.js
@@ -2,9 +2,15 @@ const accountId=4050;
 let   accountEmail="deeppadwale@gmail.come";
 var   accountPassword ="123"
       accountCity="kolhapr";
-let   accountAddress;
+let   accountAddress="pachgov road";
 
     //  accountId=5050;
     accountCity="kolhapur"
+
+     console.log(accountId);
+     console.log(accountEmail);
+     console.log(accountPassword);
+     console.log(accountAddress);
+
 
      console.table([accountId,accountEmail,accountPassword,accountCity,accountAddress])
---
+++ .devcontainer/0_1javascript/03_datatypes.js
@@ -0,0 +1,26 @@
+//  Data types in java script 
+
+     let age= 10;
+
+     let names= "Deep";
+
+     let isFalse=false; 
+
+     let homeAddress;
+
+    let temperatureRoo=null;
+
+     console.log(typeof age);
+     console.log(typeof names);
+     console.log(typeof isFalse);
+     console.log(typeof homeAddress);
+     console.log(typeof temperatureRoo);
+
+
+
+    //  number
+    // bigint
+    // string
+    // boolean
+    // undefined 
+    // null
