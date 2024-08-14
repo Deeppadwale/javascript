040dbf8bdd042b018cfa478d6c9960c97282180d
Deeppadwale <142868667+Deeppadwale@users.noreply.github.com>
2024-08-14 12:29:39 +0000

    change file location

---
+++ .devcontainer/0_1javascript/04_conversionOperation.js
@@ -0,0 +1,45 @@
+//   conversion Operation in java script
+
+//  so basicaliy we are converting a one or more data type into Number
+
+//  if you want to chak it simply change the datatype of score
+
+//   for Example  number;
+//                string;
+//                boolean;
+//                undefined;
+//                null;
+
+    
+    let score= (1,2,3)
+      
+    let changeValue=Number(score);
+
+    console.log(typeof score);
+
+    console.log(typeof changeValue);
+
+    console.log(changeValue);
+
+
+    // So basicaliy we are converting a one or more data type into String
+
+        let num=10
+    
+        let stringNum =String(num)
+
+       console.log(num);
+       console.log(typeof num);
+       console.log(typeof stringNum);
+       console.log(stringNum) 
+
+
+    // So basicaliy we are converting a one or more data type into boolean
+
+
+      let boll=null;
+      let bollNum=Boolean(boll);
+      console.log(boll);
+      console.log(typeof boll);
+      console.log(typeof bollNum);
+      console.log(bollNum);
