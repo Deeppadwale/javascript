299de335109184c98941fd87fa0784c4d0d63568
Deeppadwale <142868667+Deeppadwale@users.noreply.github.com>
2024-08-14 12:31:28 +0000

    summery of datatypes

---
+++ .devcontainer/0_1javascript/summery of datatypes.js
@@ -0,0 +1,7 @@
+// #primitive data type
+
+// 7 types of primitive datatype= String , nummber , boolean , null ,undefined,symbol ,bigint
+
+// #Nonprimitive data types
+
+//    Array, object function
