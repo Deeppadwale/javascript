29779a47ed7170fce44a64481cb8c3d208c1ba6f
Deep <deeppadawale2001@gmail.com>
2024-08-28 10:40:32 +0530

    all changes done

--- .devcontainer/0_1javascript/08_date_time.js
+++ .devcontainer/0_1javascript/08_date_time.js
@@ -1,28 +1,27 @@
 
+let myDate= new Date()
 
-// let myDate= new Date()
-
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate)
-
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate)
 
 
 
 
-// let myCreateDate= new Date();
-// console.log(myCreateDate.toLocaleString());
 
-// let myTime=  Date.now();
-//  console.log(Math.floor(Date.now()/1000));
+let myCreateDate= new Date();
+console.log(myCreateDate.toLocaleString());
+
+let myTime=  Date.now();
+ console.log(Math.floor(Date.now()/1000));
 
 
-// let mydate=new Date();
-// console.log(mydate.getMonth());
-// console.log(mydate.getTime());
-// console.log(mydate.getMonth())
-// console.log(mydate.getDay());
+let mydate=new Date();
+console.log(mydate.getMonth());
+console.log(mydate.getTime());
+console.log(mydate.getMonth())
+console.log(mydate.getDay());
 
 
         
