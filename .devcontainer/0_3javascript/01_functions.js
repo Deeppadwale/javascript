337b3537575646264b7259543adef7405d6c4ee8
Deeppadwale <142868667+Deeppadwale@users.noreply.github.com>
2024-08-28 09:55:02 +0000

    All changes done

--- .devcontainer/0_3javascript/01_functions.js
+++ .devcontainer/0_3javascript/01_functions.js
@@ -79,12 +79,45 @@ function addTable(n){
 // addTable(2)
 
 
-function addEven(num){
-    if(num%2==0){
-    console.log("This is even number")
- }
- else{
-    console.log("This is odd number")
- }
+// function addEven(num){
+//     if(num%2==0){
+//     console.log("This is even number")
+//  }
+//  else{
+//     console.log("This is odd number")
+//  }
+// }
+// addEven(num);
+
+
+let obj={
+    temperature:"Hot",
+    nature:"Good"
+};
+
+for(const newobj in obj){
+    console.log ( obj[newobj])
 }
-addEven(num);
+
+
+let myName=["iam","Deep"]
+
+for (const mynewname of myName){
+    console.log( typeof mynewname)
+}
+
+const a=[1,2,3]
+a[3]=4
+a.push(5);
+console.log(a)
+
+function showmethod(marks){
+      let mark=marks ?? 'absent'   // ?? operator return null and undefined
+       console.log(mark)
+
+}
+showmethod(24)
+showmethod()
+showmethod(5)
+
+
